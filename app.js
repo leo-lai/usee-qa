cd2f8dd1c9b3809bc4c6a87c70a19d6ab3fe348f
leolai <leolai.mail@qq.com>
2017-08-29 18:23:00 +0800

    666

--- app.js
+++ app.js
@@ -1,36 +1,48 @@
 //app.js
+import config from 'config'
+
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        console.log(res)
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
     // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+    this.login()
+  },
+  post: function(url = '', data = {}) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        method: 'POST',
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        url,
+        data,
+        success: resolve,
+        fail: reject
+      })
+    })
+  },
+  login: function () {
+    // 登录, 获取用户信息
+    wx.login({
+      success: loginRes => {
+        // 发送 loginRes.code 到后台换取 openId, sessionKey, unionId
+        wx.getUserInfo({
+          withCredentials: true,
+          success: userInfoRes => {
+            // 可以将 userInfoRes 发送给后台解码出 unionId
+            userInfoRes.code = loginRes.code
+            this.post(config.userInfo, userInfoRes).then((apiRes) => {
+              this.globalData.userInfo = apiRes.data
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+                this.userInfoReadyCallback(apiRes.data)
               }
-            }
-          })
-        }
+            }).catch((err) => {
+              console.log('获取用户信息失败！' + err)
+            })
+          }
+        })
       }
     })
   },
---
+++ pages/search/index.js
@@ -0,0 +1,19 @@
+Page({
+  data: {
+    history: {
+      data: []
+    },
+    result: {
+      loading: false,
+      data: []
+    }
+  },
+  // 清除历史记录
+  clear: function() {
+
+  },
+  // 搜索
+  search: function(e) {
+
+  }
+});
